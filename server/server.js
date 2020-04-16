0ae01b6c9d31390fa791fa686a2d069f31cc07d6
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-04-16 17:43:06 +0100

    add error and auth middleware Relates #5
    Co-authored-by: vatskan <vatskan@gmail.com>

--- server/server.js
+++ server/server.js
@@ -14,22 +14,24 @@ server.use(express.json()); //so that express knows to use JSON
 //Routes for questions
 server.get("/questions", questions.getAllQuestions);
 server.get("/questions/:id", questions.getQuestion);
-server.post("/questions", questions.postNewQuestion);
-server.put("/questions/:id", questions.updateQuestion);
-server.delete("/questions/:id", questions.deleteQuestion);
+server.post("/questions", auth.verifyUser, questions.postNewQuestion);
+server.put("/questions/:id", auth.verifyUser, questions.updateQuestion);
+server.delete("/questions/:id", auth.verifyUser, questions.deleteQuestion);
 
 //Routes for answers
 server.get("/answers", answers.getAnswers);
 
 server.get("/answers/:userId", answers.getUserAnswers);
 
-server.post("/answers/:id", answers.createNewAnswer);
+server.post("/answers/:id",  auth.verifyUser, answers.createNewAnswer);
 
-server.put("/answers/:id", answers.updateAnswer);
+server.put("/answers/:id",  auth.verifyUser, answers.updateAnswer);
 
-server.delete("/answers/:id", answers.deleteAnswer);
+server.delete("/answers/:id",  auth.verifyUser, answers.deleteAnswer);
 
 //Routes for users
 server.post("/users", users.createUser);
+
+server.use(error.errorHandler)
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
